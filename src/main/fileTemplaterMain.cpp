6fcdcdae08c92c0729b31e2e46af3c4a51081d99
ts <ts@basketbit.com>
2015-07-07 21:18:11 -0400

    project and directory creation

---
+++ src/main/fileTemplaterMain.cpp
@@ -0,0 +1,18 @@
+/*
+ * fileTemplaterMain.cpp
+ *
+ *  Created on: Jul 7, 2015
+ *      Author: schnet
+ */
+
+#include <iostream>
+
+int main ()
+{
+    // TODO !
+    std::cout << "hello, fileTemplaterMain!" << std::endl;
+
+    return 0;
+}
+
+
